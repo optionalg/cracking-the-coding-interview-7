d99236c2c52e3463569bd5b17855c95eb4007293
John Shiver <johneshiver@gmail.com>
2014-09-08 11:04:46 -0700

    Added solution without data structures

--- chapter_1/prob1.py
+++ chapter_1/prob1.py
@@ -1,5 +1,7 @@
 # implement an algo to determine if a string has all unique characters
 
+import sys
+
 
 def all_unique(input_string):
     if len(set(input_string)) != len(input_string):
@@ -9,4 +11,11 @@ def all_unique(input_string):
 
 
 def all_unique_without_data(input_string):
-    pass
+    if len(input_string) == 1:
+        return True
+    else:
+        character = input_string[0]
+        for x in input_string[1:]:
+            if character == x:
+                return False
+        return all_unique_without_data(input_string[1:])
--- chapter_1/test_1.py
+++ chapter_1/test_1.py
@@ -6,8 +6,12 @@ def test_all_unique():
     assert all_unique(string1) is True
     string2 = 'aabb'
     assert all_unique(string2) is False
+    string3 = 'abcdabcd'
+    assert all_unique(string3) is False
 
 
 def test_all_unique_no_data_structures():
     string1 = 'qwerty'
     assert all_unique_without_data(string1) is True
+    string2 = 'aabb'
+    assert all_unique_without_data(string2) is False
