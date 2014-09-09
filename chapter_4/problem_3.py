ede15fd955667423809b3b53d142545fcd18d8c1
John Shiver <johneshiver@gmail.com>
2014-09-09 13:03:13 -0700

    Added solution to problem 3

--- chapter_4/problem_3.py
+++ chapter_4/problem_3.py
@@ -21,17 +21,25 @@ class BinarySearchTree(object):
                     current.left = tree
                     searching = False
 
-if __name__ == '__main__':
-    tree1 = BinarySearchTree(4)
-    tree2 = BinarySearchTree(5)
-    tree3 = BinarySearchTree(7)
-    tree4 = BinarySearchTree(6)
-    tree5 = BinarySearchTree(3)
-    tree1.insert(tree2)
-    tree1.insert(tree3)
-    tree1.insert(tree4)
-    tree1.insert(tree5)
+
+def in_order(tree):
+    if tree is None:
+        return
+    else:
+        in_order(tree.left)
+        print tree.data
+        in_order(tree.right)
 
 
-
-
+def insert_sorted_array(input_array):
+    mid_point = len(input_array) // 2
+    tree = BinarySearchTree(input_array[mid_point])
+    insert_right = mid_point + 1
+    insert_left = mid_point -1
+    while insert_right < len(input_array):
+        tree.insert(BinarySearchTree(input_array[insert_right]))
+        insert_right += 1
+    while insert_left >= 0:
+        tree.insert(BinarySearchTree(input_array[insert_left]))
+        insert_left -= 1
+    return tree
