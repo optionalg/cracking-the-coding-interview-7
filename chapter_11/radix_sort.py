3b0eb70d236efe69521ac55b3249f8301fef9fe3
John Shiver <johneshiver@gmail.com>
2014-09-09 21:17:59 -0700

    Radix sort is passing

--- chapter_11/radix_sort.py
+++ chapter_11/radix_sort.py
@@ -4,20 +4,22 @@ def radix_sort(uList):
     sorting = True
     m = 10
     n = 1
+    calls = 0
     while sorting:
         buckets = [[] for x in range(10)]
         for i in uList:
             b = i
             b = b % m
-            b = b % n
-            i.append[buckets[b]]
+            b = b / n
+            buckets[b].append(i)
         if len(buckets[0]) == len(uList):
             sorting = False
         else:
             uList = []
             for i in range(10):
-                for n in buckets[i]:
-                    uList.append(n)
+                for x in buckets[i]:
+                    uList.append(x)
             m *= 10
             n *= 10
+            calls +=1
     return uList
