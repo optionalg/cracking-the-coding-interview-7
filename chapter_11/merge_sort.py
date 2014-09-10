19552526bb240488e24fa2eda97634847339554e
John Shiver <johneshiver@gmail.com>
2014-09-09 17:29:19 -0700

    Merge sort implementation complete

--- chapter_11/merge_sort.py
+++ chapter_11/merge_sort.py
@@ -1,34 +1,39 @@
 def merge_sort(uList):
-    if uList <= 1:
+    if len(uList) <= 1:
         return uList
     middle = len(uList) / 2
     right, left = [], []
     for n in uList[:middle]:
-        right.append(n)
-    for n in uList[middle:]:
         left.append(n)
-    right = merge_sort(right)
+    for n in uList[middle:]:
+        right.append(n)
+    print left
+    print right
     left = merge_sort(left)
-    return _merge(right, left)
+    right = merge_sort(right)
+    return _merge(left, right, uList)
 
 
-def _merge(right, left, uList):
+def _merge(left, right, uList):
     i = j = k = 0
-    while i < len(right) and j < len(left):
-        if right[i] <= left[j]:
-            uList[k] = right[i]
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            uList[k] = left[i]
             k += 1
             i += 1
         else:
-            uList[k] = left[j]
+            uList[k] = right[j]
             k += 1
             j += 1
-    while i < len(right):
-        uList[k] = right[i]
+    # tidy up, move left overs to uList
+    while i < len(left):
+        uList[k] = left[i]
         k += 1
         i += 1
-    while j < len(left):
-        uList[k] = left[k]
+    while j < len(right):
+        uList[k] = right[j]
         k += 1
         j += 1
     return uList
+
+# followed this vod: https://www.youtube.com/watch?v=TzeBrDU-JaY
